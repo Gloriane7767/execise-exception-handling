c0712ef06549c1e912f67db4ae6b798cab8d432f
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-20 23:18:11 +0100

    add exercise5

---
+++ src/main/java/Exercise5/finallyBlock.java
@@ -0,0 +1,26 @@
+package Exercise5;
+
+import java.util.Scanner;
+
+public class finallyBlock {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Enter first integer: ");
+            int num1 = scanner.nextInt();
+            System.out.print("Enter second integer: ");
+            int num2 = scanner.nextInt();
+
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        } finally {
+            System.out.println("Operation finished.");
+            scanner.close();
+        }
+
+    }
+}
