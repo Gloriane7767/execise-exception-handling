81930f94f70dcac9c1a530767677c7d6f6e2a39b
Glory Ngum Foncha <gloriane7767@gmail.com>
2026-01-20 23:58:10 +0100

    add exercise7

---
+++ src/main/java/com/gloriane/Exercise7/DuplicateNameException.java
@@ -0,0 +1,7 @@
+package com.gloriane.Exercise7;
+
+public class DuplicateNameException extends Exception {
+    public DuplicateNameException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/gloriane/Exercise7/NameNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gloriane.Exercise7;
+
+public class NameNotFoundException extends Exception {
+    public NameNotFoundException(String message) {
+        super(message);
+    }
+}
